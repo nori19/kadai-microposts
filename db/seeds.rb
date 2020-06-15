ccfe83c474958806158e0a4f19c48bab66e21983
Nori <kenso1107@outlook.jp>
2020-06-15 15:31:36 +0000

    user login

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 (1..100).each do |number|
-    User.create(name: 'taro' + number.to_s, email: 'taro' + number.to_s + '@techacademiy.jp', password_digest: 'taro' + number.to_s)
+    User.create(name: 'taro' + number.to_s, email: 'taro' + number.to_s + '@techacademy.jp', password_digest: 'tarotaro' + number.to_s)
 end
